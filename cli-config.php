259101a8aaa9d2768b539800874ac003df79019c
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-10-13 17:13:31 +0200

    remove unnecessary property

--- cli-config.php
+++ cli-config.php
@@ -2,20 +2,21 @@
 
 $containerBuilder = include __DIR__ . '/bootstrap.php';
 
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Symfony\Component\Console\Application;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 
 require_once 'bootstrap.php';
 
-$em = $containerBuilder->get('doctrine.entity_manager');
-$db = $em->getConnection();
+$entityManager = $app->getContainer()->get(EntityManager::class);
+$databaseConnection = $entityManager->getConnection();
 
 $helperSet = new \Symfony\Component\Console\Helper\HelperSet(
     [
-        'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($db),
+        'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($databaseConnection),
         'dialog' => new \Symfony\Component\Console\Helper\QuestionHelper(),
-        'em' => new EntityManagerHelper($em)
+        'em' => new EntityManagerHelper($entityManager)
     ]
 );
 
@@ -24,16 +25,5 @@ $cli->setCatchExceptions(true);
 $cli->setHelperSet($helperSet);
 
 ConsoleRunner::addCommands($cli);
-
-$cli->addCommands(
-    [
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
-        new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand()
-    ]
-);
 
 $cli->run();
